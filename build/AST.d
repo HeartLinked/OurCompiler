9bd087e326dc59e50ce72aec15ff5c6a367b9aba
HeartLinked <lfyhl0907@gmail.com>
2023-05-21 14:56:02 +0800

    finished lv7

--- build/AST.d
+++
@@ -1,5 +0,0 @@
-build/AST.o: src/AST.cpp src/AST.hpp src/DsDef.hpp
-
-src/AST.hpp:
-
-src/DsDef.hpp:
--- build/sysy.tab.d
+++
@@ -1,8 +0,0 @@
-build/sysy.tab.o: build/sysy.tab.c src/AST.hpp src/DsDef.hpp \
-  build/sysy.tab.hpp
-
-src/AST.hpp:
-
-src/DsDef.hpp:
-
-build/sysy.tab.hpp:
