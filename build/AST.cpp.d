ef4d22220cf3e08f214c83b78687b9df5d3f34f2
knightever <3083135443@qq.com>
2023-05-22 05:29:28 -0400

    lv6

--- build/AST.cpp.d
+++
@@ -1,8 +0,0 @@
-/root/compiler/Flex-Bison-Part1/build/AST.cpp.o: \
-  /root/compiler/Flex-Bison-Part1/src/AST.cpp \
-  /root/compiler/Flex-Bison-Part1/src/AST.hpp \
-  /root/compiler/Flex-Bison-Part1/src/DsDef.hpp
-
-/root/compiler/Flex-Bison-Part1/src/AST.hpp:
-
-/root/compiler/Flex-Bison-Part1/src/DsDef.hpp:
--- build/sysy.tab.cpp.d
+++
@@ -1,8 +0,0 @@
-/root/compiler/Flex-Bison-Part1/build/sysy.tab.cpp.o: \
-  /root/compiler/Flex-Bison-Part1/build/sysy.tab.cpp \
-  /root/compiler/Flex-Bison-Part1/src/AST.hpp \
-  /root/compiler/Flex-Bison-Part1/src/DsDef.hpp
-
-/root/compiler/Flex-Bison-Part1/src/AST.hpp:
-
-/root/compiler/Flex-Bison-Part1/src/DsDef.hpp:
